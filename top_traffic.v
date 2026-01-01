bea39846cb20b86f4c9619ce6053c1fa794b1a41
Navyashree-21 <navyashree4556@gmail.com>
2026-01-01 21:59:06 +0530

    Update traffic_light_top.v

--- top_traffic.v
+++ top_traffic.v
@@ -1,24 +1,55 @@
-module top_traffic(
-    input clk,       // FPGA 50 MHz clock
-    input rst,
-    output [1:0] ns, ew
+module traffic_light (
+    input clk_50mhz,
+    input  rst,
+    input emergency_A,emergency_B,
+    output A_red,
+    output A_yellow,
+    output A_green,
+    output B_red,
+    output B_yellow,
+    output B_green
+    
 );
 
-wire slow_clk;
-
-// Instantiate Clock Divider
-clock_divider #(.DIVISOR(50000000)) u1 (
-    .clk(clk),
-    .rst(rst),
-    .clk_out(slow_clk)
+    /* Internal signals */
+    wire [2:0] state,next_state;
+    wire time1;
+    wire time2;
+    clk_div_1hz u_div (
+    .clk_50mhz (clk_50mhz),
+    .rst       (rst),
+    .clk_1hz   (clk_1hz)
 );
 
-// Instantiate Traffic Light Controller
-traffic_light u2 (
-    .clk(slow_clk),
-    .rst(rst),
-    .ns(ns),
-    .ew(ew)
-);
+    /* FSM instance */
+    fsm_module f1 (
+        .clk_1hz   (clk_1hz),
+        .rst   (rst),
+        .time1 (time1),
+        .time2 (time2),
+        .state (state),
+        .emergency_A(emergency_A),
+        .emergency_B(emergency_B)
+    );
+
+    /* Timer instance */
+    timer_module t1 (
+        .clk_1hz  (clk_1hz),
+        .rst   (rst),
+        .state (state),
+        .time1 (time1),
+        .time2 (time2)
+    );
+
+    /* Output decoder instance */
+    output_module o1 (
+        .state    (state),
+        .A_red    (A_red),
+        .A_yellow (A_yellow),
+        .A_green  (A_green),
+        .B_red    (B_red),
+        .B_yellow (B_yellow),
+        .B_green  (B_green)
+    );
 
 endmodule
